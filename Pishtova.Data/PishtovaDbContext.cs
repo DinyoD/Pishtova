eabd79b36edcf271e5235f2c405ca9de51482ed5
DinyoD <40523718+DinyoD@users.noreply.github.com>
2021-09-07 21:50:50 +0300

    Working on PishtovaDbContext.

--- Pishtova.Data/PishtovaDbContext.cs
+++ Pishtova.Data/PishtovaDbContext.cs
@@ -1,8 +1,24 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace Pishtova.Data
 {
-    public class Class1
+    public class PishtovaDbContext : DbContext
     {
+        public PishtovaDbContext(DbContextOptions<PishtovaDbContext> options)
+            : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
     }
 }
