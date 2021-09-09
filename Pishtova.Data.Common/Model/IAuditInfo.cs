bcf8ef10d98dd56e4b911a05e704f038d5d5d1d9
DinyoD <40523718+DinyoD@users.noreply.github.com>
2021-09-09 20:43:56 +0300

    Adding abstract classes and interfaces for models to create.

---
+++ Pishtova.Data.Common/Model/IAuditInfo.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Pishtova.Data.Common.Model
+{
+    using System;
+
+    public interface IAuditInfo
+    {
+        DateTime CreatedOn { get; set; }
+
+        DateTime? ModifiedOn { get; set; }
+    }
+}
---
+++ Pishtova.Data.Common/Model/IDeletableEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Pishtova.Data.Common.Model
+{
+    using System;
+
+    public interface IDeletableEntity
+    {
+        bool IsDeleted { get; set; }
+
+        DateTime? DeletedOn { get; set; }
+    }
+}
