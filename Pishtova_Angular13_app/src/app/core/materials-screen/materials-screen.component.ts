659f2ab88469576681b886936ca73f695914b770
DinyoD <dinyo517@gmail.com>
2022-03-06 16:45:05 +0200

    Materials-screen UI - done.

--- Pishtova_Angular13_app/src/app/core/materials-screen/materials-screen.component.ts
+++ Pishtova_Angular13_app/src/app/core/materials-screen/materials-screen.component.ts
@@ -3,6 +3,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { AuthorModel } from 'src/app/models/author';
 import { WorkModel } from 'src/app/models/work';
+import { Authors } from 'src/app/resource/data-maps/authors';
 import { MaterialsService } from 'src/app/services/materials/materials.service';
 
 const comingSoonUrl: string = 'src/app/core/materials-screen/coming-soon-page.html';
@@ -28,6 +29,14 @@ export class MaterialsScreenComponent implements OnInit {
   public works: WorkModel[]|null = null
   public authorIndex: number|null = null;
   public workIndex: number|null = null;
+  public selectedWorkIndex: number|null = null;
+  public authorsPics: string[] =[];
+
+  public showButton: boolean = false;
+
+  public selectedAuthorInd: number|null= null;
+  public selectedWorkInd: number|null= null;
+  public selectedButton: string|null= null;
 
   constructor(
     private cd: ChangeDetectorRef,
@@ -35,6 +44,7 @@ export class MaterialsScreenComponent implements OnInit {
     private actRoute: ActivatedRoute,) { }
 
   ngOnInit(): void {
+    this.authorsPics = new Authors().pictures;
     const urlId = Number(this.actRoute.snapshot.paramMap.get('id'));
     this.materialsService.getAuthorsWithWorks(urlId).subscribe( a => this.authors = a.sort((x,y) => x.index - y.index))
   }
@@ -42,26 +52,32 @@ export class MaterialsScreenComponent implements OnInit {
   public chooseAuthor = (author: AuthorModel): void => {
     this.works = author.works.sort((x,y) => x.index - y.index);
     this.authorIndex = author.index;
+    this.selectedWorkInd = null;
   }
 
   public chooseWork = (workIndex: number):void => {
     this.workIndex = workIndex;
+    this.selectedWorkInd = workIndex;
     this.handlerTextBtn();
+    this.showButton = true;
   }
 
   public handlerTextBtn = ():void => {
     this.url = this.setUrl();
     this.cd.detectChanges();
+    this.selectedButton = 'text';
   }
 
   public handlerAnaliseBtn = ():void => {
     this.url = this.setUrl(2);
     this.cd.detectChanges();
+    this.selectedButton = 'analize';
   }
 
   public handlerExtrasBtn = ():void => {
     this.url =this.setUrl(3);
     this.cd.detectChanges();
+    this.selectedButton = 'extras';
   }
 
   private setUrl = (htmlNumber: number = 1): string => {
---
+++ Pishtova_Angular13_app/src/app/resource/data-maps/authors.ts
@@ -0,0 +1,19 @@
+export class Authors {
+    public readonly pictures: string[] = [
+        '../../../assets/authorsPictures/Botev.jpg',
+        '../../../assets/authorsPictures/Vazov.jpg',
+        '../../../assets/authorsPictures/Konstantinov.jpg',
+        '../../../assets/authorsPictures/Slaveykov.jpg',
+        '../../../assets/authorsPictures/Yavorov.jpg',
+        '../../../assets/authorsPictures/Debelianov.jpg',
+        '../../../assets/authorsPictures/Smirnenski.jpg',
+        '../../../assets/authorsPictures/Milev.jpg',
+        '../../../assets/authorsPictures/Dalchev.jpg',
+        '../../../assets/authorsPictures/Bagriana.jpg',
+        '../../../assets/authorsPictures/Pelin.jpg',
+        '../../../assets/authorsPictures/Yovkov.jpg',
+        '../../../assets/authorsPictures/Vapcarov.jpg',
+        '../../../assets/authorsPictures/Talev.jpg',
+        '../../../assets/authorsPictures/Dimov.jpg',
+    ]
+}
