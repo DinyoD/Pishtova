3df7cf4ad442d9c3ff7bf0d76822b770c6130e1a
DinyoD <dinyo517@gmail.com>
2022-04-07 20:24:36 +0300

    Install and test Stripe into API.

---
+++ Pishtova_.NET5.0_API/Pishtova_ASP.NET_web_api/Controllers/PaymentsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Stripe;
+
+namespace Pishtova_ASP.NET_web_api.Controllers
+{
+    public class PaymentsController: ApiController
+    {
+		[HttpGet("products")]
+		public IActionResult Products()
+		{
+
+			StripeConfiguration.ApiKey = "sk_test_51KlsUjBd9uAKWbJc1M1IAR6barYfnAUHC3FTwm8uVoqObQ2s2SuipPzOIs4O88kx6Bf3hmFOytnYFqedkrOnq2je00EDSSc9de";
+
+			var options = new ProductListOptions
+			{
+				Limit = 3,
+			};
+			var service = new ProductService();
+			StripeList<Product> products = service.List(
+			  options
+			);
+
+
+			return Ok(products);
+		}
+	}
+}
