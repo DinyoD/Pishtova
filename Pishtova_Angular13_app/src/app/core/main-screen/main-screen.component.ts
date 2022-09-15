d4865c6cf298e0c1f0f3b9222c2b737d40462bb2
DinyoD <dinyo517@gmail.com>
2022-09-15 16:31:22 +0300

    Update subjects view in main-screen (mobile).

--- Pishtova_Angular13_app/src/app/core/main-screen/main-screen.component.ts
+++ Pishtova_Angular13_app/src/app/core/main-screen/main-screen.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { SubjectModel } from 'src/app/models/subject/subject';
@@ -9,24 +9,40 @@ import { SubjectService} from '../../services';
   templateUrl: './main-screen.component.html',
   styleUrls: ['./main-screen.component.css']
 })
-export class MainScreenComponent{
+export class MainScreenComponent implements OnInit{
 
-  public subjects: Observable<SubjectModel[]> = this.subjectService.getAllSubjects()
+  public subjects: Observable<SubjectModel[]> = this.subjectService.getAllSubjects();;
 
   constructor(
     private subjectService: SubjectService, 
     private router : Router 
     ) {
-      this.subjectService.settingSubjectModel(null);
-    }
-
-  chooseSubject(sbj: SubjectModel): void{
+      this.subjectService.setSubject(null);
+  }
+  ngOnInit(): void {
     this.subjectService.subjectChanged.subscribe(s =>{ 
       if (s) {   
         this.router.navigate([`subject/${s?.id}`]);
       }
-    });
-    this.subjectService.settingSubjectModel(sbj);
+    });;
+  }
+  public chooseSubject(sbj: SubjectModel): void{
+    this.subjectService.setSubject(sbj);
+  }
+
+  // TODO Optimize method!!
+  public setClassBySbjName(name: string): string {
+    let result: string = '';
+    switch (name) {
+      case "Биология":
+        result = "bio"
+        break;
+      case "Български език":
+        result = "bel"
+        break;
+    }
+
+    return result;
   }
 
 }
