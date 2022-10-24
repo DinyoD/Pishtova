8347c0d051bbea5bfab305df735457542b5752a4
DinyoD <dinyo517@gmail.com>
2022-10-24 17:04:51 +0300

    Add extension class CollectionExtensions.

---
+++ Pishtova_.NET _6_API/Pishtova.Data.Common/Extensions/CollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Pishtova.Data.Common.Extensions
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public static class CollectionExtensions
+    {
+        public static IEnumerable<T> OrEmptyIfNull<T>(this IEnumerable<T> collection) 
+            => collection ?? Enumerable.Empty<T>();
+
+        public static IEnumerable<T> IgnoreNullValues<T>(this IEnumerable<T> collection) 
+            where T : class
+        {
+            if (collection is null) throw new ArgumentNullException(nameof(collection));
+            return collection.Where(x => x is not null);
+        }
+    }
+}
