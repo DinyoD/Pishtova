40740606ec04820be12f32c68ae7458a8dd6cefe
DinyoD <dinyo517@gmail.com>
2022-10-25 17:37:18 +0300

    Refactoring TestsController.

--- Pishtova_.NET _6_API/Pishtova_ASP.NET_web_api/Controllers/TestsController.cs
+++ Pishtova_.NET _6_API/Pishtova_ASP.NET_web_api/Controllers/TestsController.cs
@@ -9,8 +9,9 @@ namespace Pishtova_ASP.NET_web_api.Controllers
     using Pishtova.Data.Model;
     using Pishtova.Services.Data;
     using Pishtova_ASP.NET_web_api.Model.Test;
-    using Pishtova_ASP.NET_web_api.Model.Results;
-    using Pishtova_ASP.NET_web_api.Model.OperationResults;
+    using Pishtova.Data.Common.Utilities;
+    using Pishtova_ASP.NET_web_api.Extensions;
+    using System;
 
     public class TestsController : ApiController
     {
@@ -28,41 +29,59 @@ namespace Pishtova_ASP.NET_web_api.Controllers
             this.usersBadgesService = usersBadgesService;
         }
 
+
+        [HttpGet("{testId:int}")]
+        [HttpHead("{testId:int}")]
+        public async Task<IActionResult> GetById([FromRoute] int testId)
+        {
+            var getTestResult = await this.testService.GetAsync(testId);
+            if (!getTestResult.IsSuccessful) return this.Error(getTestResult);
+
+            var test = getTestResult.Data;
+            if (test is null) return this.NotFound();
+
+            return this.Ok(test);
+        }
+
         [HttpPost]
         [Route("[action]")]
         public async Task<IActionResult> Create([FromBody] TestInputModel inputModel)
         {
-            try
-            {
-                
-                var test = new Test { 
-                    UserId = inputModel.UserId,
-                    SubjectId = inputModel.SubjectId,
-                    Score = inputModel.Score,
-                };
+            var operationResult = new OperationResult();
+            if (!operationResult.ValidateNotNull(inputModel)) return this.Error(operationResult);
 
-                var testId = await this.testService.CreateAsync(test);
-                await this.SaveBadgeForTestCount(inputModel.UserId, testId);
-                return StatusCode(201, new SaveTestResult { TestId = testId });
-            }
-            catch (System.Exception)
-            {
-                return StatusCode(400, new ErrorResult { Message = "Server error" });
-            }
+            //TODO Validate inputModel (FluentValidation)
+
+            //TODO Implement Mapper (AutoMapper)
+            var test = new Test { 
+                UserId = inputModel.UserId,
+                SubjectId = inputModel.SubjectId,
+                Score = inputModel.Score,
+            };
+
+            var createdTestResult = await this.testService.CreateAsync(test);
+            if (!createdTestResult.IsSuccessful) return this.Error(createdTestResult);
+
+            var createdTestId = createdTestResult.Data;
+            await this.SaveBadgeForTestCount(inputModel.UserId, createdTestId);
+
+            return this.CreatedAtAction("GetById", new { testId = createdTestId}, new { testId = createdTestId});
+
         }
 
 
         // TODO Optimaze method!!!
         private async Task SaveBadgeForTestCount(string userId, int testId)
         {
-            var userTests = this.testService.GetUserTestsCount(userId);
+            var result = await this.testService.GetUserTestsCountAsync(userId);
+            var userTestCount = result.Data;
             var testCountForBadge = new int[] { 10, 20, 50, 100 };
-            if (!testCountForBadge.Contains(userTests))
+            if (!testCountForBadge.Contains(userTestCount))
             {
                 return;
             }
-            var badgeCode = 2000 + userTests;
-            var badge = await this.badgeService.GetAllByCodeAsync(badgeCode);
+            var badgeCode = 2000 + userTestCount;
+            var badge = await this.badgeService.GetByCodeAsync(badgeCode);
             var model = new UserBadge
             {
                 UserId = userId,
