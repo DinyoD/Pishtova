73e4d2008d24bfebeb6d05cd5f4ee5151aabd518
DinyoD <dinyo517@gmail.com>
2022-10-25 17:36:37 +0300

    Implement ControllerExtensions.

---
+++ Pishtova_.NET _6_API/Pishtova_ASP.NET_web_api/Extensions/ControllerExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace Pishtova_ASP.NET_web_api.Extensions
+{
+    using System;
+    using System.Linq;
+
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+
+    using Pishtova.Data.Common.Utilities;
+
+    public static class ControllerExtensions
+    {
+        public static ActionResult Error(this ControllerBase controller, OperationResult operationResult)
+        {
+            if (controller is null) throw new ArgumentNullException(nameof(controller));
+            if (operationResult is null) throw new ArgumentNullException(nameof(operationResult));
+
+            var statusCode = operationResult.Errors.Any(e => e.IsNotExpected) ? StatusCodes.Status500InternalServerError : StatusCodes.Status400BadRequest;
+            return controller.Problem(operationResult.ToString(), controller.Request.Path, statusCode, "Your actions was not executed successfully.");
+        }
+    }
+}
