778d474db2dd64f99d382a7d631bbdc990bd2d59
DinyoD <dinyo517@gmail.com>
2022-10-28 15:03:42 +0300

    Add comments in interfaces.

--- Pishtova_.NET _6_API/Pishtova.Services.Data/IBadgeService.cs
+++ Pishtova_.NET _6_API/Pishtova.Services.Data/IBadgeService.cs
@@ -2,10 +2,17 @@
 {
     using System.Threading.Tasks;
 
+    using Pishtova.Data.Model;
     using Pishtova.Data.Common.Utilities;
 
     public interface IBadgeService
     {
-        Task<OperationResult<string>> GetIdByCodeAsync(int badgeCode);
+        /// <summary>
+        /// Asynchronously find the entity(Badge) with the given code value.
+        /// </summary>
+        /// <param name="code">The values of the code property for the entity to be found.</param>
+        /// <returns>A task whose result contains OperationResult object with Data property - founded entity(Badge). If no entity is found, then
+        ///     Data is null.</returns>
+        Task<OperationResult<Badge>> GetByCodeAsync(int code);
     }
 }
--- Pishtova_.NET _6_API/Pishtova.Services.Data/ITestService.cs
+++ Pishtova_.NET _6_API/Pishtova.Services.Data/ITestService.cs
@@ -6,10 +6,23 @@
 
     public interface ITestService
     {
-        Task<OperationResult<Test>> GetAsync(int testId);
-
+        /// <summary>
+        /// Asynchronously find an entity(Test) with the given primary key(Id) value.
+        /// </summary>
+        /// <param name="id">The values of the primary key(Id) for the entity to be found.</param>
+        /// <returns>A task whose result contains OperationResult object with Data property - founded entity(Test). If no entity is found, then
+        ///     Data is null</returns>
+        /// <summary>
+        /// Asynchronously save entity(Test) in Db.
+        /// </summary>
+        /// <param name="test"></param>
+        /// <returns>A task whose result contains OperationResult object with Data property - primary key(Id) value of the newly created test</returns>
         Task<OperationResult<int>> CreateAsync(Test test);
-
-        Task<OperationResult<int>> GetUserTestsCountAsync(string userId);
+        /// <summary>
+        /// Asynchronously get count of completed test for a user.
+        /// </summary>
+        /// <param name="id">The unique user Id.</param>
+        /// <returns>A task whose result contains OperationResult object with Data property - count of all completed tests for a user.</returns>
+        Task<OperationResult<int>> GetUserTestsCountAsync(string id);
     }
 }
