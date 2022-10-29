80814a0f8b72caed20fbdbe8b172cf80ff838eab
DinyoD <dinyo517@gmail.com>
2022-10-29 09:52:42 +0300

    Update main-screen with some stats for user.

--- Pishtova_Angular13_app/src/app/core/main-screen/main-screen.component.ts
+++ Pishtova_Angular13_app/src/app/core/main-screen/main-screen.component.ts
@@ -3,7 +3,9 @@ import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 import { SubjectModel } from 'src/app/models/subject/subject';
-import { AuthService, SubjectService, TestService } from '../../services';
+import { TestByDaysModel } from 'src/app/models/test/testsByDays';
+import { TestScoreModel } from 'src/app/models/test/testScore';
+import { AuthService, StatsService, SubjectService } from '../../services';
 import { HtmlHelper } from "../helpers/subjectHelper";
 
 @Component({
@@ -16,10 +18,12 @@ export class MainScreenComponent implements OnInit{
   public subjects: Observable<SubjectModel[]> = this.subjectService.getAllSubjects();
   public userTestCount: number|null = null;
   public userId: string|null = null;
+  public userLastTests: TestScoreModel[]|null = null;
+  public userLastDays: TestByDaysModel[]|null = null;
 
   constructor(
     private subjectService: SubjectService, 
-    private testService: TestService,
+    private statsService: StatsService,
     private authService: AuthService,
     private router : Router 
   ) {
@@ -29,8 +33,11 @@ export class MainScreenComponent implements OnInit{
   ngOnInit(): void {
     this.subjectService.subjectChanged.subscribe(sbjModel => { if(sbjModel) this.router.navigate([`subject/${sbjModel?.id}`])});
     const user = this.authService.getCurrentUser();
-    if (user) this.userId = user.id;
-    if(this.userId) this.testService.getUserTestsCount(this.userId).subscribe(x => this.userTestCount = x)
+    if( user == null ) return;
+    this.userId = user.id;
+    this.statsService.getTestCount(this.userId).subscribe(x => this.userTestCount = x);
+    this.statsService.getLastTests(this.userId, 10).subscribe(x => this.userLastTests = x);
+    this.statsService.getTestsByDays(this.userId, 7).subscribe(x => this.userLastDays = x);
   }
 
   public chooseSubject(sbj: SubjectModel): void{
