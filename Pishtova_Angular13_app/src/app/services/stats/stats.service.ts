b45706630c0948382e1e29723752eb2d4edd9453
DinyoD <dinyo517@gmail.com>
2022-11-01 20:52:08 +0200

    Update stats.service.

--- Pishtova_Angular13_app/src/app/services/stats/stats.service.ts
+++ Pishtova_Angular13_app/src/app/services/stats/stats.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { TestByDaysModel } from 'src/app/models/test/testsByDays';
 import { TestScoreModel } from 'src/app/models/test/testScore';
+import { UserPointsForSubjectModel } from 'src/app/models/user/userPointBySubject';
 import { environment as env } from '../../../environments/environment';
 
 @Injectable({
@@ -19,10 +20,18 @@ export class StatsService {
     }
 
     public getLastTests = (userId: string, testsCount: number): Observable<TestScoreModel[]> => {
-      return this.httpClient.get<TestScoreModel[]>(env.API_URL + `/userstats/lasttests/?userId=${userId}&testsCount=${testsCount}`);
+      return this.httpClient.get<TestScoreModel[]>(env.API_URL + `/userstats/lasttests?userId=${userId}&testsCount=${testsCount}`);
     }
 
     public getTestsByDays = (userId: string, daysCount: number): Observable<TestByDaysModel[]> => {
-      return this.httpClient.get<TestByDaysModel[]>(env.API_URL + `/userstats/lastdays/?userId=${userId}&daysCount=${daysCount}`);
+      return this.httpClient.get<TestByDaysModel[]>(env.API_URL + `/userstats/lastdays?userId=${userId}&daysCount=${daysCount}`);
+    }
+
+    public getSubjectRanking = (subjectId: number|null): Observable<UserPointsForSubjectModel[]> => {
+      return this.httpClient.get<UserPointsForSubjectModel[]>(env.API_URL + `/userstats/subjectrank?subjectId=${subjectId}`)
+    }
+
+    public getUserBestRank = (userId: string): Observable<Object> => {
+      return this.httpClient.get<Object>(env.API_URL + `/userstats/bestrank?userId=${userId}`)
     }
   }
