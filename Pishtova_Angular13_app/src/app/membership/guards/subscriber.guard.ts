093eb81c869b6ff366d2cc198598f3121decd808
DinyoD <dinyo517@gmail.com>
2022-11-15 13:37:16 +0200

    Update logic in subscriber.guard.

--- Pishtova_Angular13_app/src/app/membership/guards/subscriber.guard.ts
+++ Pishtova_Angular13_app/src/app/membership/guards/subscriber.guard.ts
@@ -16,14 +16,11 @@ export class ForSubscribersGuard implements CanActivate{
     next: ActivatedRouteSnapshot, 
     stte: RouterStateSnapshot
     ): boolean {
-        console.log(this.authService.getCurrentUser());
-        
         let suscriber = this.authService.getCurrentUser()?.isSubscriber;
-    if (!suscriber) {
-        console.log(123);      
+    if (suscriber) {    
         return true;
     }
-    this.router.navigate(['/memberships/plan']);
+    this.router.navigate(['/memberships']);
     return false;
     };
 
