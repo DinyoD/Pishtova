3136834defeb96796b013eaf40f3de520b72c623
Dinyo <dinyo517@gmail.com>
2022-12-01 14:42:56 +0200

    Update Town service.

--- Pishtova_Angular13_app/src/app/services/town/town.service.ts
+++ Pishtova_Angular13_app/src/app/services/town/town.service.ts
@@ -13,6 +13,6 @@ export class TownService {
   constructor(private httpClient: HttpClient) { }
 
   public getTownsByMunicipalityId = (municipalityId: number): Observable<TownModel[]> => {
-    return this.httpClient.get<TownModel[]>(env.API_URL + `/towns/bymunicipality/${municipalityId}`)
+    return this.httpClient.get<TownModel[]>(env.API_URL + `/towns/municipality/${municipalityId}`)
   }
 }
