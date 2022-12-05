dc6ec0438a202a5bd2a8e479b7b9ab710c4e0a69
Dinyo <dinyo517@gmail.com>
2022-12-05 18:14:29 +0200

    Update GlobalConstants file.

--- Pishtova_.NET _6_API/Pishtova.Common/GlobalConstants.cs
+++ Pishtova_.NET _6_API/Pishtova.Common/GlobalConstants.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-
-namespace Pishtova.Common
+﻿namespace Pishtova.Common
 {
     public class GlobalConstants
     {
@@ -13,6 +11,18 @@ namespace Pishtova.Common
         public const string BiologyBgName = "Биология";
 
         public const string BulgarianBgName = "Български език";
+
+        public const string GeographyBgName = "География";
+
+        public const string EnglishBgName = "Английски език";
+
+        public const string BiologyId = "bio";
+
+        public const string BulgarianId = "bel";
+
+        public const string GeographyId = "geo";
+
+        public const string EnglishId = "eng";
 
         public static readonly string[] BiologyCategoriesName = {
             "Равнища, поведение",
