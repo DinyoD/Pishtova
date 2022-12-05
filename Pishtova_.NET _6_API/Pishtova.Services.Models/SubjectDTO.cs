ec8ab3837f62ec7029b9d288831a4b498c2fdc65
Dinyo <dinyo517@gmail.com>
2022-12-05 18:19:58 +0200

    Update SubjectId type in models.

--- Pishtova_.NET _6_API/Pishtova.Services.Models/SubjectDTO.cs
+++ Pishtova_.NET _6_API/Pishtova.Services.Models/SubjectDTO.cs
@@ -4,6 +4,8 @@
 
     public class SubjectDTO
     {
+        public string Id { get; set; }
+
         public string Name { get; set; }
 
         public ICollection<SubjectCategoryDTO> Categories { get; set; }
