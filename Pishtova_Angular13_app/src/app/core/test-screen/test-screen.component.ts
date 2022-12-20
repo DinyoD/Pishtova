28e0d3ac15cdb290ae615bd5ec3145790b3857c9
Dinyo <dinyo517@gmail.com>
2022-12-20 20:30:04 +0200

    Update test-screen component.

--- Pishtova_Angular13_app/src/app/core/test-screen/test-screen.component.ts
+++ Pishtova_Angular13_app/src/app/core/test-screen/test-screen.component.ts
@@ -1,35 +1,35 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-
-import { ProblemModel } from 'src/app/models/problem/problem';
-import { AnswerModel } from 'src/app/models/answer';
-import { ProblemScoreModel } from 'src/app/models/problem/problemScore';
-import { ProblemService, PointsService, TestService, BadgesService, UserService } from 'src/app/services'
-import { GreetingDialogComponent } from 'src/app/shared/greeting-dialog/greeting-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
-import { getBadgeCodeByPoints } from 'src/app/resource/data';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+
+import { AnswerModel } from 'src/app/models/answer';
+import { ProblemModel } from 'src/app/models/problem/problem';
+import { TestToSaveModel } from 'src/app/models/test/testToSave';
+import { ProblemScoreModel } from 'src/app/models/problem/problemScore';
+import { ProblemService, PointsService, TestService, UserService } from 'src/app/services';
 import { ISaveTestResult } from 'src/app/models/operation.result/saveTest';
 import { ImageDialogComponent } from 'src/app/shared/image-dialog/image-dialog.component';
-import { BadgeToSaveModel } from 'src/app/models/badge/badgeToSave';
-import { TestToSaveModel } from 'src/app/models/test/testToSave';
+import { GreetingDialogComponent } from 'src/app/shared/greeting-dialog/greeting-dialog.component';
 
 @Component({
   selector: 'app-test-screen',
   templateUrl: './test-screen.component.html',
   styleUrls: ['./test-screen.component.css']
 })
+
 export class TestScreenComponent implements OnInit {
 
   public subjectId: string | null = null;
 
   public problems: ProblemModel[] = [];
+  public problemsCount: number = 0;
   public problemNumber: number = 1;
   public userId: string|undefined;
 
   public someAnswerIsClicked: boolean = false;
   public selectedAnswerId: string|null = null
   public points: number = 0;
-  public maxScore: number = 20;
+  public maxScore: number = 0;
   public navigationName: string = 'Тест';
   public navigationUrl: string = '';
 
@@ -37,7 +37,6 @@ export class TestScreenComponent implements OnInit {
     private problemService: ProblemService,
     private pointsService: PointsService,
     private testService: TestService,
-    private badgeService: BadgesService,
     private userService: UserService,
     private dialog: MatDialog,
     private cd: ChangeDetectorRef,
@@ -50,11 +49,15 @@ export class TestScreenComponent implements OnInit {
     }
 
     ngOnInit(): void {
-      this.problemService.generateTestBySubjectId(this.subjectId).subscribe(problems => this.problems = problems);
+      this.problemService.generateTestBySubjectId(this.subjectId).subscribe(problems => {
+        this.problems = problems;
+        this.problemsCount =  problems.length;
+        this.maxScore = problems.length;
+      });
       this.pointsService.clearPoints();
       this.pointsService.pointsChanged.subscribe(p => { 
         this.points = p;
-        this.maxScore = p + 20 - this.problemNumber;
+        this.maxScore = p + this.problems.length - this.problemNumber;
       });
       this.userId = this.userService.getCurrentUser()?.id;
     }
@@ -70,22 +73,23 @@ export class TestScreenComponent implements OnInit {
         subjectCategoryId: subjectCategoryId,
         points: selectedAnswer.isCorrect ? 1 : 0
       }
+      this.pointsService.addPoints(problemScoreModel.points);
       this.pointsService.saveScore(problemScoreModel).subscribe(() => {
           if (problemScoreModel.points == 0 ) {
             return;
           }
           const points = this.pointsService.gettingPoints();
           switch (points) {
-          case 14:
+          case this.problemsCount*0.7:
             this.dialog.open( GreetingDialogComponent, { data:  70})
             break;
-          case 16:
+          case this.problemsCount*0.8:
             this.dialog.open( GreetingDialogComponent, { data:  80})
           break;   
-          case 18:
+          case this.problemsCount*0.9:
             this.dialog.open( GreetingDialogComponent, { data:  90})
             break;
-          case 20:
+          case this.problemsCount:
             this.dialog.open( GreetingDialogComponent, { data:  100})
           break; 
           default:
@@ -109,12 +113,9 @@ export class TestScreenComponent implements OnInit {
       const testToSave = this.getTestToSave();
       if (!testToSave) return;
 
-      this.testService.saveTest(testToSave).subscribe( (res: ISaveTestResult) => {          
-        const winnedBadge = this.getWinnedBadge(res.testId);
-        winnedBadge !== null ? this.badgeService.saveBadge(winnedBadge)
-                                                .subscribe(() => this.router.navigate(['/subject/' + this.subjectId +'/result'], {state: {testId: res.testId}}))
-                             : this.router.navigate(['/subject/' + this.subjectId +'/result'], {state: {testId: res.testId}});
-      });
+      this.testService.saveTest(testToSave).subscribe( (res: ISaveTestResult) => 
+        this.router.navigate(['/subject/' + this.subjectId +'/result'], {state: {testId: res.testId}})
+      );
 
     }
 
@@ -127,22 +128,12 @@ export class TestScreenComponent implements OnInit {
       })
     };
 
-    private getWinnedBadge(testId: number): BadgeToSaveModel|null {
-      const code = getBadgeCodeByPoints().get(this.points);
-      if(!code) return null;
-      const userId = this.userService.getCurrentUser()?.id;
-      if (!userId) return null;
-      const badge: BadgeToSaveModel = {
-        userId: userId,
-        testId: testId,
-        badgeCode: code,
-      };
-      return badge;
-    }
 
     private getTestToSave(): TestToSaveModel|null {;
+
       if (!this.userId || this.subjectId == null) return null;
-      const score = Math.trunc(this.points / 20 * 100);
+
+      const score = Math.trunc(this.points / this.problemsCount * 100);
 
       return {
         userId: this.userId,
@@ -152,7 +143,6 @@ export class TestScreenComponent implements OnInit {
     }
 
     private nextStepIsValid = (): boolean => this.someAnswerIsClicked && (this.subjectId != null);
+    
   }
   
-
-
