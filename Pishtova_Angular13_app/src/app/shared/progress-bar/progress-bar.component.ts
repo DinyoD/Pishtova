283fc1dfd436e183eadd9287616f85a10b0c3c59
Dinyo <dinyo517@gmail.com>
2023-02-05 14:01:10 +0200

    Update progress-bar component.

--- Pishtova_Angular13_app/src/app/shared/progress-bar/progress-bar.component.ts
+++ Pishtova_Angular13_app/src/app/shared/progress-bar/progress-bar.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-progress-bar',
   templateUrl: './progress-bar.component.html',
   styleUrls: ['./progress-bar.component.css']
 })
-export class ProgressBarComponent implements OnInit {
+export class ProgressBarComponent implements OnInit, OnChanges {
 
   @Input() count: number|null = null;
   @Input() problemNumber: number = 1;
@@ -15,7 +15,10 @@ export class ProgressBarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.countArray = Array(this.count).fill(1).map((x, i) => i + 1);
+    this.countArray = Array(this.count).fill(1).map((x, i) => i + 1); 
   }
 
+  ngOnChanges(){
+    this.countArray = Array(this.count).fill(1).map((x, i) => i + 1); 
+  }
 }
