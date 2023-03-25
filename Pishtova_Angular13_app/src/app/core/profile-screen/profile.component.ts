fb936dddd043ed38ce56b50acb0b3caf25c676bc
Dinyo <dinyo517@gmail.com>
2023-03-25 17:48:13 +0200

    Add delete user functionality.

--- Pishtova_Angular13_app/src/app/core/profile-screen/profile.component.ts
+++ Pishtova_Angular13_app/src/app/core/profile-screen/profile.component.ts
@@ -7,12 +7,18 @@ import { environment as env } from 'src/environments/environment';
 import { UploadFileDialogComponent } from 'src/app/shared/upload-file-dialog/upload-file-dialog.component';
 import { ConfirmationDialogComponent } from 'src/app/shared/confirmation-dialog/confirmation-dialog.component';
 import { UpdateProfileInfoDialogComponent } from 'src/app/shared/update-profile-info-dialog/update-profile-info-dialog.component';
-import { AuthService, SubjectService, UserService, BadgesService, PointsService } from 'src/app/services';
+import {
+  AuthService,
+  SubjectService,
+  UserService,
+  BadgesService,
+  PointsService,
+} from 'src/app/services';
 import { EditProfileModel } from 'src/app/models/profile/editProfile';
 import { ChangeProfileInfoModel } from 'src/app/models/profile/changeProfileInfo';
 import { UpdateEmailModel } from 'src/app/models/profile/updateEmail';
 import { ProfileModel } from 'src/app/models/profile/profile';
-import { SubjectInfo } from "src/app/models/subject/subjectInfo";
+import { SubjectInfo } from 'src/app/models/subject/subjectInfo';
 import { ConfirmationDialogModel } from 'src/app/shared/confirmation-dialog/confirmation-dialog';
 import { BadgesCountModel } from 'src/app/models/badge/badgesCount';
 import { SubjectWithCategories } from 'src/app/models/subjectCategory/subjectWithcategories';
@@ -22,17 +28,16 @@ import { UpdatePictureModel } from 'src/app/models/profile/updatePicture';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
-
-  public currentUser: CurrentUserModel|null = null;
-  public profile: ProfileModel|null = null;
-  public profileSchoolId: number|null = null; 
+  public currentUser: CurrentUserModel | null = null;
+  public profile: ProfileModel | null = null;
+  public profileSchoolId: number | null = null;
   public showDetails: boolean = false;
-  public subjectWithCategories: SubjectWithCategories|null = null;
-  public badges: BadgesCountModel[]|null = null;
-  public subjectsInfo: SubjectInfo[]|null = null;
+  public subjectWithCategories: SubjectWithCategories | null = null;
+  public badges: BadgesCountModel[] | null = null;
+  public subjectsInfo: SubjectInfo[] | null = null;
 
   constructor(
     private authService: AuthService,
@@ -41,51 +46,71 @@ export class ProfileComponent implements OnInit {
     private badgeService: BadgesService,
     private pointsService: PointsService,
     private dialog: MatDialog,
-    private router: Router) { 
-
-      this.subjectService.setSubject(null);
-      this.currentUser = this.userService.getCurrentUser();
+    private router: Router
+  ) {
+    this.subjectService.setSubject(null);
+    this.currentUser = this.userService.getCurrentUser();
   }
 
   ngOnInit(): void {
-    if(this.currentUser == null) return;
-    this.userService.getUserProfile(this.currentUser.id).subscribe( profile => {
-      this.profile = profile;
-      this.profileSchoolId = profile.school.id;
-      this.pointsService.getPointsBySubjects(profile.id).subscribe(res => this.subjectsInfo = res);
-     });
-    this.badgeService.getUserBadges(this.currentUser.id).subscribe(res => this.badges = res);
+    if (this.currentUser == null) return;
+    this.userService
+      .getUserProfile(this.currentUser.id)
+      .subscribe((profile) => {
+        this.profile = profile;
+        this.profileSchoolId = profile.school.id;
+        this.pointsService
+          .getPointsBySubjects(profile.id)
+          .subscribe((res) => (this.subjectsInfo = res));
+      });
+    this.badgeService
+      .getUserBadges(this.currentUser.id)
+      .subscribe((res) => (this.badges = res));
   }
 
   public badgeCount = (code: number): number => {
-    const badgeModel =this.badges?.find(x => x.code == code);
-    return  badgeModel == undefined 
-                ? 0 
-                : badgeModel.count;
-  }
+    const badgeModel = this.badges?.find((x) => x.code == code);
+    return badgeModel == undefined ? 0 : badgeModel.count;
+  };
 
-  public showSubjectDetails = (subjectId: string, subjectName: string, subjectPoints: number, subjectProblems: number): void => { 
+  public showSubjectDetails = (
+    subjectId: string,
+    subjectName: string,
+    subjectPoints: number,
+    subjectProblems: number
+  ): void => {
     if (!this.profile) return;
-    this.showDetails= true;
-    this.pointsService.getPointsBySubjectCategories(this.profile?.id, subjectId)
-      .subscribe( res => 
-        this.subjectWithCategories = {
-        id: subjectId,
-        name: subjectName,
-        points: subjectPoints,
-        problems: subjectProblems,
-        categories: res.map(x => {return {...x, percentage: Math.round(x.points / x.problems * 100)}})
-      });
-  }
+    this.showDetails = true;
+    this.pointsService
+      .getPointsBySubjectCategories(this.profile?.id, subjectId)
+      .subscribe(
+        (res) =>
+          (this.subjectWithCategories = {
+            id: subjectId,
+            name: subjectName,
+            points: subjectPoints,
+            problems: subjectProblems,
+            categories: res.map((x) => {
+              return {
+                ...x,
+                percentage: Math.round((x.points / x.problems) * 100),
+              };
+            }),
+          })
+      );
+  };
 
   public closeSubjectDetails = (): void => {
     this.subjectWithCategories = null;
     this.showDetails = false;
-  }
+  };
 
   public changePicture = (): void => {
-    this.dialog.open(UploadFileDialogComponent).afterClosed().subscribe( imageUrl => this.updatePicture(imageUrl));
-  }
+    this.dialog
+      .open(UploadFileDialogComponent)
+      .afterClosed()
+      .subscribe((imageUrl) => this.updatePicture(imageUrl));
+  };
 
   public changeInfo = (): void => {
     if (this.profile == null) {
@@ -96,96 +121,128 @@ export class ProfileComponent implements OnInit {
       name: this.profile.name,
       email: this.profile.email,
       grade: this.profile.grade,
-      school: this.profile.school
-    }
+      school: this.profile.school,
+    };
 
     this.dialog
-      .open(UpdateProfileInfoDialogComponent, {autoFocus: false, data: profileToEdit})
+      .open(UpdateProfileInfoDialogComponent, {
+        autoFocus: false,
+        data: profileToEdit,
+      })
       .afterClosed()
-      .subscribe( 
-       (updatedProfile) => {
-          if (updatedProfile == null) return;
+      .subscribe((updatedProfile) => {
+        if (updatedProfile == null) return;
 
-          const infoChanged = profileToEdit.name != updatedProfile.name ||
-                              profileToEdit.grade != updatedProfile.grade ||
-                              this.profileSchoolId != updatedProfile.schoolId;
+        const infoChanged =
+          profileToEdit.name != updatedProfile.name ||
+          profileToEdit.grade != updatedProfile.grade ||
+          this.profileSchoolId != updatedProfile.schoolId;
 
-          const emailChanged = profileToEdit.email.toUpperCase() != updatedProfile.email.toUpperCase();
-          
-          if(infoChanged)
-          {
-            this.updateInfo(updatedProfile.name, updatedProfile.grade, updatedProfile.schoolId);
-          };
+        const emailChanged =
+          profileToEdit.email.toUpperCase() !=
+          updatedProfile.email.toUpperCase();
 
-          if(emailChanged)
-          { 
-            this.changeUserEmail(updatedProfile.email);
-          }
+        if (infoChanged) {
+          this.updateInfo(
+            updatedProfile.name,
+            updatedProfile.grade,
+            updatedProfile.schoolId
+          );
+        }
 
+        if (emailChanged) {
+          this.changeUserEmail(updatedProfile.email);
+        }
+      });
+  };
+
+  public deleteUserPermanently = (): void => {
+    const dialogData = new ConfirmationDialogModel(
+      'Моля, потвърдете изтриване!',
+      '*вашата информация ще се изтрие изцяло!'
+    );
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      closeOnNavigation: true,
+      data: dialogData,
+    });
+
+    dialogRef.afterClosed().subscribe((dialogResult) => {
+      if (dialogResult) {
+        if (!this.currentUser?.id) return;
+        this.authService.delete(this.currentUser.id).subscribe((res) => {
+          console.log(res);
         });
-  }
+        this.authService.logout();
+        this.router.navigate(['/auth/register']);
+      }
+    });
+  };
 
-  private updatePicture = (imageUrl: string|null) => {
+  private updatePicture = (imageUrl: string | null) => {
+    if (this.currentUser == null || imageUrl == null) return;
 
-    if(this.currentUser == null || imageUrl == null) return;
-
-    const dataModel: UpdatePictureModel  ={
+    const dataModel: UpdatePictureModel = {
       id: this.currentUser.id,
-      pictureUrl : imageUrl
-    }
+      pictureUrl: imageUrl,
+    };
     this.userService.updatePictureUrl(dataModel).subscribe(() => {
       this.userService.updateAvatar(imageUrl);
       this.reload();
-    })
-  }
+    });
+  };
 
-  private updateInfo = (name: string, grade: number, schoolId: number): void =>  {
-    if(this.currentUser == null){
+  private updateInfo = (
+    name: string,
+    grade: number,
+    schoolId: number
+  ): void => {
+    if (this.currentUser == null) {
       return;
     }
     const dataModel: ChangeProfileInfoModel = {
       id: this.currentUser.id,
       name: name,
       grade: grade,
-      schoolId: schoolId
+      schoolId: schoolId,
     };
 
-    this.userService.updateInfo(dataModel).subscribe( () => this.reload());
-  }
+    this.userService.updateInfo(dataModel).subscribe(() => this.reload());
+  };
 
-  private changeUserEmail = (email: string): void =>  {
+  private changeUserEmail = (email: string): void => {
     this.confirmEmailChange(email).subscribe((confirmed: boolean) => {
-        if (confirmed) this.updateEmail(email);
-      });
-  }
+      if (confirmed) this.updateEmail(email);
+    });
+  };
 
   private confirmEmailChange = (email: string): Observable<boolean> => {
-    const dialogData = new ConfirmationDialogModel(`Променяте мейлът си на ${email}`,
-    '*ще получите линк за потвърждение');
+    const dialogData = new ConfirmationDialogModel(
+      `Променяте мейлът си на ${email}`,
+      '*ще получите линк за потвърждение'
+    );
 
-   const dialogObs =   this.dialog
-    .open(ConfirmationDialogComponent, { data: dialogData })
-    .afterClosed();
+    const dialogObs = this.dialog
+      .open(ConfirmationDialogComponent, { data: dialogData })
+      .afterClosed();
 
     return dialogObs;
-  }
+  };
 
   private updateEmail = (email: string): void => {
-    if(this.currentUser == null){
+    if (this.currentUser == null) {
       return;
     }
     const dataModel: UpdateEmailModel = {
       id: this.currentUser.id,
       email: email,
-      clientURI: env.CLIENT_URI + `/auth/emailconfirmation`
+      clientURI: env.CLIENT_URI + `/auth/emailconfirmation`,
     };
-    this.userService.updateEmail(dataModel)
-    .subscribe(() => {
+    this.userService.updateEmail(dataModel).subscribe(() => {
       this.authService.logout();
       this.router.navigate(['/']);
     });
-  }
+  };
 
-  private reload = () => this.router.navigate(['/']).then(() => this.router.navigate(['/profile']));
-
+  private reload = () =>
+    this.router.navigate(['/']).then(() => this.router.navigate(['/profile']));
 }
--- Pishtova_Angular13_app/src/app/services/auth/auth.service.ts
+++ Pishtova_Angular13_app/src/app/services/auth/auth.service.ts
@@ -8,65 +8,74 @@ import { UserForLoginModel } from '../../authentication/models/userForLogin';
 import { UserForRegistrationModel } from '../../authentication/models/userForRegistration';
 import { ForgotPasswordModel } from '../../authentication/models/forgotPassword';
 import { ResetPasswordModel } from '../../authentication/models/resetPassword';
-import  ILoginResult  from '../../authentication/models/results/LoginResult';
+import ILoginResult from '../../authentication/models/results/LoginResult';
 import { environment as env } from 'src/environments/environment';
-import { Storage} from '../../utilities/constants/storage';
+import { Storage } from '../../utilities/constants/storage';
 import { StorageService } from '..';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  private _authChangeSub: Subject<boolean> = new Subject<boolean>()
+  private _authChangeSub: Subject<boolean> = new Subject<boolean>();
   public isAuthChange: Observable<boolean> = this._authChangeSub.asObservable();
 
   constructor(
-    private httpClient: HttpClient, 
+    private httpClient: HttpClient,
     private jwtHelper: JwtHelperService,
     private storage: StorageService
-    ) { }
+  ) {}
 
   public register = (user: UserForRegistrationModel): Observable<Object> => {
     return this.httpClient.post(env.API_URL + '/identity/register', user);
-  }
+  };
 
-  
+  public delete = (id: string): Observable<Object> => {
+    return this.httpClient.delete(env.API_URL + `/identity/delete/${id}`);
+  };
+
   public login = (user: UserForLoginModel): Observable<ILoginResult> => {
-    return this.httpClient.post<ILoginResult>(env.API_URL + `/identity/login`, user)
-  }
+    return this.httpClient.post<ILoginResult>(
+      env.API_URL + `/identity/login`,
+      user
+    );
+  };
 
   public logout = () => {
     this.storage.clear();
     this.sendAuthStateChangeNotification(false);
-  }
+  };
 
   public confirmEmail = (token: string, email: string): Observable<Object> => {
-    let params = new HttpParams({ encoder: new CustomEncoder() })
+    let params = new HttpParams({ encoder: new CustomEncoder() });
     params = params.append('token', token);
     params = params.append('email', email);
-    return this.httpClient.get(env.API_URL + `/identity/emailconfirmation`,{ params: params})
-  }
+    return this.httpClient.get(env.API_URL + `/identity/emailconfirmation`, {
+      params: params,
+    });
+  };
 
   public forgotPassword = (body: ForgotPasswordModel): Observable<Object> => {
     return this.httpClient.post(env.API_URL + '/identity/forgotpassword', body);
-  }
+  };
 
   public resetPassword = (body: ResetPasswordModel): Observable<Object> => {
-    return this.httpClient.post(env.API_URL + '/identity/resetpassword', body)
-  }
+    return this.httpClient.post(env.API_URL + '/identity/resetpassword', body);
+  };
 
   public isUserAuthenticated = (): boolean => {
     const token = this.storage.getItem<string>(Storage.TOKEN);
     return token != null && !this.jwtHelper.isTokenExpired(token);
-  }
-  
+  };
+
   public setAuthState = (token: string): void => {
-    if(token) this.storage.setItem(Storage.TOKEN, token);
+    if (token) this.storage.setItem(Storage.TOKEN, token);
     this.sendAuthStateChangeNotification(token != null);
-  }
-  
-  private sendAuthStateChangeNotification = (isAuthenticated: boolean): void => {
+  };
+
+  private sendAuthStateChangeNotification = (
+    isAuthenticated: boolean
+  ): void => {
     this._authChangeSub.next(isAuthenticated);
-  }
+  };
 }
